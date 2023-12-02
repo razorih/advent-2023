00474f4807ccbbdb4d404a1c767da98afae4e9fb
Jani Purhonen <razorih@users.noreply.github.com>
2023-12-02 10:50:09 +0200

    Day 2 - Part 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -43,9 +43,7 @@ impl FromStr for Game {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let input = read_input()?;
-
+fn silver(input: &str) -> anyhow::Result<usize> {
     let mut possible_sum = 0;
     'games: for line in input.trim().lines() {
         let game: Game = line.parse()?;
@@ -60,7 +58,40 @@ fn main() -> anyhow::Result<()> {
         possible_sum += game.id;
     }
 
-    println!("Silver: {}", possible_sum);
+    Ok(possible_sum)
+}
+
+fn gold(input: &str) -> anyhow::Result<usize> {
+    let mut power_sum: usize = 0;
+    for line in input.trim().lines() {
+        let game: Game = line.parse()?;
+
+        let mut max = (0, 0, 0);
+        for set in game.sets {
+            if set.0 > max.0 {
+                max.0 = set.0;
+            }
+
+            if set.1 > max.1 {
+                max.1 = set.1;
+            }
+
+            if set.2 > max.2 {
+                max.2 = set.2;
+            }
+        }
+
+        power_sum += max.0 as usize * max.1 as usize * max.2 as usize;
+    }
+
+    Ok(power_sum)
+}
+
+fn main() -> anyhow::Result<()> {
+    let input = read_input()?;
+
+    println!("Silver: {}", silver(input.as_str())?);
+    println!("  Gold: {}", gold(input.as_str())?);
 
     Ok(())
 }
