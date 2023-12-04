46511cc9238afd2531010c74de429b80a47999b3
Jani Purhonen <razorih@users.noreply.github.com>
2023-12-04 17:03:34 +0200

    Day 4 - Further optimizations
    - Store numbers in two u64 integers, avoiding HashSet completely.
      This is possible due to the fact that cards only have numbers between
      1 and 99, which can be stored in said 128-bit bitset.
    - Optimize prefix stripping as card number can be discarded.
    - Counting matching numbers is now implemented using binary ops.
    
    Lowers runtime on bigboy from 500ms -> 300ms

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,20 +1,38 @@
-use std::collections::HashSet;
-
 use advent::read_input;
 
+/// Bitset for storing integers between 0-127
 #[derive(Debug)]
-struct MatchCountIter<T> {
-    linesource: T,
-    winning: HashSet<u16>,
-}
+struct NumberBitSet(u64, u64);
 
-impl<T> MatchCountIter<T> {
-    fn from_linesource(linesource: T) -> Self {
-        Self {
-            linesource,
-            winning: HashSet::new(),
+impl NumberBitSet {
+    fn from_iter(iter: impl Iterator<Item = u8>) -> Self {
+        let mut set = Self(0, 0);
+        for num in iter {
+            set.insert(num);
+        }
+        set
+    }
+
+    fn count_matching_numbers(&self, other: &Self) -> u32 {
+        (self.0 & other.0).count_ones() + (self.1 & other.1).count_ones()
+    }
+
+    fn insert(&mut self, item: u8) {
+        debug_assert!(item < 100);
+
+        // Check which integer stores this item
+        if item < 64 { // 0-63
+            self.0 |= 1 << item
+        } else { // 64-127
+            self.1 |= 1 << (item - 64)
         }
     }
+}
+
+#[derive(Debug)]
+struct MatchCountIter<T>(T);
+impl<T> MatchCountIter<T> {
+    fn from_linesource(linesource: T) -> Self { Self(linesource) }
 }
 
 impl<'a, T> Iterator for MatchCountIter<T>
@@ -23,33 +41,24 @@ where
 {
     type Item = usize;
     fn next(&mut self) -> Option<Self::Item> {
-        self.winning.clear();
-
-        if let Some(line) = self.linesource.next() {
-            let s = line.strip_prefix("Card ")?;
-            let (_, rest) = s.split_once(':')?;
+        if let Some(line) = self.0.next() {
+            let (_, rest) = line.split_once(':')?;
             let (winning, have) = rest.split_once('|')?;
 
             // Only extend winning set, as we can check for membership separately.
             // Note: Set is empty, but the same allocation is reused in order
             //       to avoid reallocating on each time next() is called.
-            self.winning.extend(
+            let winning = NumberBitSet::from_iter(
                 winning.split_ascii_whitespace()
-                    .map(|num| num.parse::<u16>().unwrap())
+                    .map(|num| num.parse::<u8>().unwrap())
             );
 
-            // Check for membership in a loop instead of building a separate set.
-            let mut match_count: usize = 0;
-            for num in have
-                .split_ascii_whitespace()
-                .map(|num| num.parse::<u16>().unwrap())
-            {
-                if self.winning.contains(&num) {
-                    match_count += 1;
-                }
-            }
+            let have = NumberBitSet::from_iter(
+                have.split_ascii_whitespace()
+                    .map(|num| num.parse::<u8>().unwrap())
+            );
 
-            Some(match_count)
+            Some(winning.count_matching_numbers(&have) as usize)
         } else {
             None
         }
