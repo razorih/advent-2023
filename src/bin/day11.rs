18841addef871470e1d9e6c718c094b3f9259479
Jani Purhonen <razorih@users.noreply.github.com>
2023-12-11 14:37:18 +0200

    Day 11 - Part 2
    Silver can be solved by parsing the input with `GALAXY_SIZE = 1`

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -2,7 +2,7 @@ use advent::read_input;
 use grid::Grid;
 
 
-fn parse(s: &str) -> Vec<(usize, usize)> {
+fn parse<const GALAXY_SIZE: usize>(s: &str) -> Vec<(usize, usize)> {
     let mut grid: Vec<char> = Vec::new();
 
     let cols = s.lines().nth(0).unwrap().len();
@@ -10,7 +10,7 @@ fn parse(s: &str) -> Vec<(usize, usize)> {
         grid.extend(line.chars())
     }
 
-    let mut grid = Grid::from_vec(grid, cols);
+    let grid = Grid::from_vec(grid, cols);
 
     fn galaxy_filter<'a, T: Iterator<Item = &'a char> + Clone>(
         (col, content): (usize, T)
@@ -21,33 +21,49 @@ fn parse(s: &str) -> Vec<(usize, usize)> {
             None
         }
     }
-    // Expand columns and rows
+
+    // Find all empty column and row indices.
+    // Resulting index arrays are sorted.
     let empty_cols: Vec<usize> = grid.iter_cols()
         .enumerate().filter_map(galaxy_filter).collect();
-
     let empty_rows: Vec<usize> = grid.iter_rows()
         .enumerate().filter_map(galaxy_filter).collect();
 
     println!("empty cols: {empty_cols:?}, rows: {empty_rows:?}");
 
-    // The `fix` term below accounts for the fact that inserting a column/row
-    // pushes later columns'/rows' indices by one
-
-    let n_rows = grid.rows();
-    for (col, fix) in empty_cols.into_iter().zip(0_usize..) {
-        grid.insert_col(col+fix, vec!['.'; n_rows]);
-    }
-
-    let n_cols = grid.cols(); // changed from expanding columns
-    for (row, fix) in empty_rows.into_iter().zip(0_usize..) {
-        grid.insert_row(row+fix, vec!['.'; n_cols]);
-    }
-
-    // Galaxy has been expanded, return galaxy coordinates
-    Vec::from_iter(
+    // Collect a list of unexpanded galaxies
+    let mut galaxies = Vec::from_iter(
         grid.indexed_iter()
             .filter_map(|(coords, &ch)| if ch == '#' { Some(coords) } else { None })
-    )
+    );
+
+    // Expand galaxies using a "scanline".
+    // Columns and rows are expanded separately.
+    //
+    // Loop through all empty column/row and check if any galaxies appears after them.
+    // If so, move it forward accordingly.
+    // 
+    // Since moving a galaxy also moves following empty galaxies, we have to keep
+    // a counter (`fix`), which is used to calculate the "true" position
+    // where galaxy should be expanded.
+    
+    for (empty, fix) in empty_cols.into_iter().zip(0_usize..) {
+        for galaxy in &mut galaxies {
+            if galaxy.1 >= empty + fix*GALAXY_SIZE {
+                galaxy.1 = galaxy.1 + GALAXY_SIZE;
+            }
+        }
+    }
+
+    for (empty, fix) in empty_rows.into_iter().zip(0_usize..) {
+        for galaxy in &mut galaxies {
+            if galaxy.0 >= empty + fix*GALAXY_SIZE {
+                galaxy.0 = galaxy.0 + GALAXY_SIZE;
+            }
+        }
+    }
+
+    galaxies
 }
 
 /// Distance function measuring distance between two galaxies.
@@ -58,9 +74,9 @@ fn dist((x1, y1): (usize, usize), (x2, y2): (usize, usize)) -> usize {
 
 fn main() -> anyhow::Result<()> {
     let input = read_input()?;
-    let galaxies = parse(&input);
+    let galaxies = parse::<999_999>(&input);
 
-    let mut silver_sum = 0;
+    let mut sum = 0;
     for i in 0..galaxies.len() {
         for j in i..galaxies.len() {
             if i == j {
@@ -68,12 +84,12 @@ fn main() -> anyhow::Result<()> {
             }
 
             let dist = dist(galaxies[i], galaxies[j]);
-            silver_sum += dist;
+            sum += dist;
             // println!("{i} -> {j} dist: {}", dist)
         }
     }
 
-    println!("Silver: {:?}", silver_sum);
+    println!("Gold: {:?}", sum);
 
     Ok(())
 }
