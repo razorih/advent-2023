7cb881350a9c0364a8c8276337ae44eec8e49a34
Jani Purhonen <razorih@users.noreply.github.com>
2023-12-13 16:31:35 +0200

    Day 13 - Part 2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -14,24 +14,33 @@ fn solve(pattern: &Grid<Tile>) -> Reflection {
     let (rows, cols) = pattern.size();
 
     // Look at all neighboring columns and rows and check if we cant start mirror there
-    // Returned `pivot` here is already corrected for the 1-based indexing
+    // Returned `pivot`s here are already corrected for the 1-based indexing
 
     for (i, j) in (0..cols-1).zip(1..cols) {
-        if let Some(pivot) = check_expanding(|col| pattern.iter_col(col), i, j, pattern.cols()-1) {
-            println!("found mirror at col pair {i}-{j}, pivot: {pivot:?}");
-            return Reflection::Column(pivot)
+        if let Some((pivot, true)) = check_expanding(
+            |col| pattern.iter_col(col),
+            i,
+            j,
+            pattern.cols()-1,
+        ) {
+            println!("found mirror at [COLUMN] pivot: {pivot:?} (smudged)");
+            return Reflection::Column(pivot);
         }
-
     }
 
     for (i, j) in (0..rows-1).zip(1..rows) {
-        if let Some(pivot) = check_expanding(|row| pattern.iter_row(row), i, j, pattern.rows()-1) {
-            println!("found mirror at row pair {i}-{j}, pivot: {pivot:?}");
-            return Reflection::Row(pivot)
+        if let Some((pivot, true)) = check_expanding(
+            |row| pattern.iter_row(row),
+            i,
+            j,
+            pattern.rows()-1
+        ) {
+            println!("found mirror at [ROW] pivot: {pivot:?} (smudged)");
+            return Reflection::Row(pivot);
         }
     }
 
-    unreachable!("all patterns should have one mirror");
+    unreachable!("patterns should have exactly one mirror");
 }
 
 fn main() -> anyhow::Result<()> {
@@ -47,7 +56,7 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    println!("Silver: {}", sum);
+    println!("Gold: {}", sum);
 
     Ok(())
 }
@@ -72,6 +81,70 @@ fn parse_patterns(s: &str) -> Vec<Grid<Tile>> {
     }
 
     out
+}
+
+
+/// Check for reflection by iteratively expanding two indices.
+/// 
+/// Example how `i` and `j` move, each column (or similarly a row) must match.
+/// in order for the iterator to continue. If either `i` or `j` reach array 
+/// bounds, the array has a mirror which's pivot is at the original `i` index.
+/// ```not_rust
+///     ij
+/// #.##..##.
+/// ---------
+///    i  j
+/// #.##..##.
+/// ---------
+///   i    j
+/// #.##..##.
+/// ```
+fn check_expanding<F, I>(
+    source: F,
+    mut i: usize,
+    mut j: usize,
+    max_j: usize,
+) -> Option<(usize, bool)>
+where
+    F: Fn(usize) -> I,
+    I: Iterator,
+    I::Item: PartialEq,
+{
+    let pivot = i;
+    let mut smudge_cleaned = false;
+
+    let res = loop {
+        // Check for reflection between arrays given by i and j
+        
+        // Count how many tiles differ between arrays
+        let differing = source(i).zip(source(j))
+            .filter(|(el_i, el_j)| el_i != el_j)
+            .count();
+        
+        // If there is only one difference and we haven't cleaned any smudges
+        // yet, we can clean this one.
+        if !smudge_cleaned && differing == 1 {
+            smudge_cleaned = true;
+            // Continue on as normal
+        } else if differing != 0 {
+            // Arrays cannot be cleaned because there's either
+            // - too much to clean
+            // - smudge has already been cleaned
+            // therefore, this is not a mirror
+            break None
+        }
+
+        // If we would go out-of-bounds next iteration, this is a mirror
+        if i == 0 || j == max_j {
+            break Some((pivot + 1, smudge_cleaned))
+        }
+
+        // Otherwise, expand the search and repeat
+        i -= 1;
+        j += 1;
+    };
+
+    res
 }
 
 fn print(pattern: &Grid<Tile>) {
@@ -120,50 +193,4 @@ impl Tile {
             _   => None
         }
     }
-}
-
-/// Check for reflection by iteratively expanding two indices.
-/// 
-/// Example how `i` and `j` move, each column (or similarly a row) must match.
-/// in order for the iterator to continue. If either `i` or `j` reach array 
-/// bounds, the array has a mirror which's pivot is at the original `i` index.
-/// ```not_rust
-///     ij
-/// #.##..##.
-/// ---------
-///    i  j
-/// #.##..##.
-/// ---------
-///   i    j
-/// #.##..##.
-/// ```
-fn check_expanding<F, I>(
-    source: F,
-    mut i: usize,
-    mut j: usize,
-    max_j: usize,
-) -> Option<usize>
-where
-    F: Fn(usize) -> I,
-    I: Iterator,
-    I::Item: PartialEq,
-{
-    let pivot = i;
-    let res = loop {
-        // Check for reflection between arrays given by i and j
-        if !source(i).eq(source(j)) {
-            break None
-        }
-
-        // If we would go out-of-bounds next iteration, this is a mirror
-        if i == 0 || j == max_j {
-            break Some(pivot + 1)
-        }
-
-        // Otherwise, expand search and repeat
-        i -= 1;
-        j += 1;
-    };
-
-    res
 }
