d2aed289bf7a4f6d938255dcbc4a740f117f180e
Jani Purhonen <razorih@users.noreply.github.com>
2023-12-14 17:25:58 +0200

    Day 14 - Part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,9 +1,26 @@
+use std::collections::HashMap;
+
 use advent::read_input;
 use grid::Grid;
 
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum Tile { Empty, Round, Cube }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction { North, West, South, East }
+
+impl Direction {
+    /// Advance to next direction.
+    fn next(&mut self) -> Self {
+        match self {
+            Self::North => Self::West,
+            Self::West  => Self::South,
+            Self::South => Self::East,
+            Self::East  => Self::North,
+        }
+    }
+}
 
 /// Swaps two elements of given grid.
 /// 
@@ -17,63 +34,151 @@ unsafe fn swap(grid: &mut Grid<Tile>, x: (usize, usize), y: (usize, usize)) {
     }
 }
 
-fn tick(grid: Grid<Tile>) -> (Grid<Tile>, usize) {
-    let mut out = grid.clone();
-    let mut tiles_moved = 0;
+/// Calculate next position based on `direction`.
+fn next_position(pos: (usize, usize), direction: Direction) -> Option<(usize, usize)> {
+    Some(match direction {
+        Direction::North => (pos.0.checked_sub(1)?, pos.1),
+        Direction::West  => (pos.0, pos.1.checked_sub(1)?),
+        Direction::South => (pos.0 + 1, pos.1),
+        Direction::East  => (pos.0, pos.1 + 1),
+    })
+}
 
-    for ((row, col), &tile) in grid.indexed_iter() {
-        if tile == Tile::Round {
-            // Look north for some space
-            let Some(north_row) = row.checked_sub(1) else {
-                continue; // Out of bounds, continue
-            };
-            
-            match grid.get(north_row, col) {
-                Some(Tile::Empty) => {
-                    // SAFETY: `out` is a clone of `grid`, and thus has the same bounds as `grid`.
-                    // Nothing in this function changes those bounds.
-                    // `grid` (and `out`) element access is valid for (row, col) and (row-1, col).
-                    unsafe { swap(&mut out, (row, col), (row-1, col)) };
-                    tiles_moved += 1;
-                },
-                _ => continue, // May be out-of-bounds or blockage
-            }
+struct Puzzle {
+    puzzle_main: Grid<Tile>,
+    puzzle_temp: Grid<Tile>,
+    seen: HashMap<Vec<Tile>, usize>,
+    cycle_length: usize,
+    loads: Vec<usize>,
+}
+
+impl Puzzle {
+    fn new(puzzle: Grid<Tile>) -> Self {
+        Self {
+            puzzle_main: puzzle.clone(),
+            puzzle_temp: puzzle,
+            seen: HashMap::new(),
+            cycle_length: 0,
+            loads: Vec::new(),
         }
     }
 
-    (out, tiles_moved)
+    fn get(&self) -> &Grid<Tile> {
+        &self.puzzle_main
+    }
+
+    fn print(&self) {
+        print_puzzle(&self.puzzle_main).unwrap();
+    }
+
+    /// Ticks boulders on the board to some [`Direction`].
+    /// 
+    /// Returns how many boulders were moved.
+    fn tick(&mut self, direction: Direction) -> usize {
+        let mut tiles_moved = 0;
+
+        for ((row, col), &tile) in self.puzzle_main.indexed_iter() {
+            if tile == Tile::Round {
+                // Look north for some space
+                let Some((next_row, next_col)) = next_position((row, col), direction) else {
+                    continue; // Out of bounds, continue
+                };
+                
+                match self.puzzle_main.get(next_row, next_col) {
+                    Some(Tile::Empty) => {
+                        // SAFETY: `temp` is a clone of `main`, and thus has the same bounds as `main`.
+                        // Nothing in this function changes those bounds.
+                        // `main` (and `temp`) element access is valid for (row, col) and (next_row, next_col).
+                        unsafe {
+                            swap(
+                                &mut self.puzzle_temp,
+                                (row, col),
+                                (next_row, next_col)
+                            )
+                        };
+                        tiles_moved += 1;
+                    },
+                    _ => continue, // May be out-of-bounds or blockage
+                }
+            }
+        }
+
+        // `temp` now has a board where all boulders have moved
+        // Copy it into `main` in case there's a next iteration
+        self.puzzle_temp.clone_into(&mut self.puzzle_main);
+
+        tiles_moved
+    }
+}
+
+impl Iterator for &mut Puzzle {
+    type Item = (usize, usize, bool);
+
+    /// Evaluates one "cycle"
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut direction = Direction::North;
+        loop {
+            let tiles_moved = self.tick(direction);
+
+            if tiles_moved == 0 {
+                direction = direction.next();
+
+                // We've moved back to start, cycle is complete
+                if direction == Direction::North {
+                    let load = calculate_load(self.get());
+                    // Check if we have seen this before
+                    // sadly, we can't get immutable ref to the underlying vec
+                    // so we have to clone the whole grid
+                    let oof = self.puzzle_main.clone().into_vec();
+
+                    self.cycle_length += 1;
+                    self.loads.push(load);
+                    if let Some(&cached_cycle) = self.seen.get(&oof) {
+                        println!("seen this grid before! at cycle {}, loop length {}", cached_cycle, self.cycle_length - cached_cycle);
+                        return Some((load, cached_cycle, true));
+                    } else {
+                        self.seen.insert(oof, self.cycle_length);
+                    }
+
+                    return Some((load, self.cycle_length, false));
+                }
+            }
+        }
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let input = read_input()?;
-    let mut puzzle = parse(&input);
+    let mut puzzle = Puzzle::new(parse(&input));
 
-    print_puzzle(&puzzle)?;
-    loop {
-        let (_puzzle, tiles_moved) = tick(puzzle);
-        puzzle = _puzzle;
-        println!("moved {tiles_moved} tiles");
+    puzzle.print();
 
-        if tiles_moved == 0 {
+    let mut loop_start = 0;
+    let mut loop_length = 0;
+
+    for ((_, int_cycle, cycle_found), cycle) in &mut puzzle.zip(1..) {
+        // println!("cycle {cycle:<5} load: {load:?}");
+        if cycle_found {
+            loop_start = int_cycle;
+            loop_length = cycle;
             break;
         }
     }
-    print_puzzle(&puzzle)?;
-    println!("Silver: {}", calculate_load(&puzzle));
+
+    let offset = (1_000_000_000 - (loop_start + 1)) % (loop_length - loop_start);
+    println!("offset {offset}, answer index: {}, answer: {}", loop_start + offset, puzzle.loads[loop_start + offset]);
+    println!("Gold: {}", puzzle.loads[loop_start + offset]);
 
     Ok(())
 }
 
 fn calculate_load(puzzle: &Grid<Tile>) -> usize {
     let mut load = 0;
-    let rows = puzzle.rows();
+    let n_rows = puzzle.rows();
 
-    for (row, load_multiplier) in puzzle.iter_rows().zip((1..=rows).rev()){
-        for &tile in row {
-            if tile == Tile::Round {
-                load += load_multiplier;
-            }
-        }
+    for (row, load_multiplier) in puzzle.iter_rows().zip((1..=n_rows).rev()) {
+        let rocks_on_row = row.filter(|&&tile| tile == Tile::Round).count();
+        load += rocks_on_row * load_multiplier;
     }
 
     load
